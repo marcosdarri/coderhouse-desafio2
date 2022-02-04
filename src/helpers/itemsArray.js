a103d4286e2bc43f0a132dab94e7f189447ad783
marcosdarri <marcosdarri@hotmail.com>
2022-02-04 12:37:45 -0300

    Agregamos info al itemsArray

--- src/helpers/itemsArray.js
+++ src/helpers/itemsArray.js
@@ -1,58 +1,58 @@
 const items = [
   {
     id: "1",
-    title: "Flores",
-    description: "Descripcion Uno",
-    precio: "$1",
-    categoria: "flor",
+    title: "Tulipan Violeta",
+    description: "Este tipo de tulipan es el mas raro de nuestro vivero, proviene del este de Vietnam.",
+    precio: "$150",
+    categoria: "tulipan",
     pictureUrl:
-      "https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/5e723bc25cafe8a3f89605a5/flores_0.jpg",
+      "https://www.floreshermosas.top/wp-content/uploads/2019/05/tulipan-morado.jpg",
   },
   {
     id: "2",
-    title: "Flores",
-    description: "Descripcion Dos",
-    precio: "$2",
-    categoria: "flor",
+    title: "Tulipan Rosada",
+    description: "Este tipo de tulipan es el mas solicitado por nuestros clientes, aprovecha nuestra promocion de 3x2!",
+    precio: "$99",
+    categoria: "tulipan",
     pictureUrl:
-      "https://s1.eestatic.com/2020/06/23/curiosidades/naturaleza-planeta-tierra/ciencias_naturales-flores-tierra_-planeta_499960581_154305912_1706x960.jpg",
+      "https://economiacircularverde.com/wp-content/uploads/2019/09/significado-de-los-tulipanes-6-300x225.jpg.webp",
   },
   {
     id: "3",
-    title: "Flores",
-    description: "Descripcion Tres",
-    precio: "$3",
-    categoria: "flor",
+    title: "Tulipan Blanco",
+    description: "Este tipo de tulipan es ideal para casamientos y eventos festivos.",
+    precio: "$120",
+    categoria: "tulipan",
     pictureUrl:
-      "https://cdn.pixabay.com/photo/2012/03/01/00/55/garden-19830__340.jpg",
+      "https://economiacircularverde.com/wp-content/uploads/2019/09/rBVaSVuHs7-AZusVAAGUk3mG6gU046-300x300.jpg.webp",
   },
 
   {
     id: "4",
-    title: "Flores",
-    description: "Descripcion Uno",
-    precio: "$4",
+    title: "Rosa Roja",
+    description: "Sin lugar a dudas el producto mas solicitado de todo nuestro vivero, especial para regalos de San Valentin!",
+    precio: "$75",
     categoria: "rosa",
     pictureUrl:
-      "https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/5e723bc25cafe8a3f89605a5/flores_0.jpg",
+      "https://www.mundoflores.net/imagenes/las-rosas-735x450.jpg",
   },
   {
     id: "5",
-    title: "Flores",
-    description: "Descripcion Dos",
-    precio: "$5",
+    title: "Rosa Azul",
+    description: "Este tipo de rosa es extremadamente rara de coseguir, nos enorgullecemos de decir que somos el unico vivero de Montevideo cultivandolas en este momento.",
+    precio: "$199",
     categoria: "rosa",
     pictureUrl:
-      "https://s1.eestatic.com/2020/06/23/curiosidades/naturaleza-planeta-tierra/ciencias_naturales-flores-tierra_-planeta_499960581_154305912_1706x960.jpg",
+      "https://t1.uc.ltmcdn.com/images/5/7/0/img_como_hacer_rosas_azules_naturales_43075_600.jpg",
   },
   {
     id: "6",
-    title: "Flores",
-    description: "Descripcion Tres",
-    precio: "$6",
+    title: "Rosa Amarilla",
+    description: "Aprovecha para pedir tu Rosa Amarilla por tiempo limitado!",
+    precio: "$85",
     categoria: "rosa",
     pictureUrl:
-      "https://cdn.pixabay.com/photo/2012/03/01/00/55/garden-19830__340.jpg",
+      "https://s1.eestatic.com/2020/06/03/curiosidades/significado/flores-plantas-curiosidades_494962046_153098316_1024x576.jpg",
   },
 ];
 
